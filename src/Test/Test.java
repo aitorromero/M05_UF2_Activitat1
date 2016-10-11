4c527bbca8a376bae21432488367c02a71ed7e9f
ALUMNEDAM <ALUMNEDAM@192.168.120.112>
2016-10-11 20:51:50 +0200

    Cambio Aitor

---
+++ src/Test/Test.java
@@ -0,0 +1,14 @@
+package Test;
+
+import myutils.MyUtils;
+
+public class Test {
+
+    public static void main(String[] args) {
+        MyUtils.edat(27, 2, 1996);
+        MyUtils.edat(27, 2, 1000);
+        MyUtils.edat(27, 2, 3000);
+        MyUtils.edat(11, 10, 2016);
+    }
+    
+}
--- src/myutils/MyUtils.java
+++ src/myutils/MyUtils.java
@@ -4,51 +4,78 @@
  */
 package myutils;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
- * @author Lluis
- * Classe de funcions pròpies utils.
+ * @author Lluis Classe de funcions pròpies utils.
  */
 public class MyUtils {
-    
 
-/**
- * 
- * @param cadena Strint cadena que es vol invertir
- * @return cadena invertida (null per cadenes nulls).
- */
-public static String inverteix (String cadena)
-{
-    String resultat="";
-    return resultat;
-}
-/**
- * 
- * @param day int dia del naixement
- * @param month int  mes del naixement
- * @param year int any del naixement
- * @return edat de la persona, per edat>150 retorna -1, per dates impossibles retorna -2
- * 
- */
-public static int edat (int day, int month, int year)
-{
-    int resultat=0;
-    return resultat;
-}
+    /**
+     *
+     * @param cadena Strint cadena que es vol invertir
+     * @return cadena invertida (null per cadenes nulls).
+     */
+    public static String inverteix(String cadena) {
+        String resultat = "";
+        if(!cadena.equals(resultat)){
+            for (int i = cadena.length()-1; i >=0; i--) {
+                resultat=resultat + cadena.charAt(i);
+            }
+        }
+        return resultat;
+    }
 
-/**
- * 
- * @param numero número del que es calcula el factorial
- * @return retorna el factorial d'un número. Si el nombre es negatiu retorna -1.
- */
-public static double factorial (double numero) {
- 
-if (numero==0)
-return 1;
-else
-{
-double resultat = numero * factorial(numero-1);
-return resultat;
-}
-}
+    /**
+     *
+     * @param day int dia del naixement
+     * @param month int mes del naixement
+     * @param year int any del naixement
+     * @return edat de la persona, per edat>150 retorna -1, per dates
+     * impossibles retorna -2
+     *
+     */
+    public static int edat(int day, int month, int year) {
+        int resultat = 0;
+        int año = Calendar.getInstance().get(Calendar.YEAR)-year;
+        int mes = Calendar.getInstance().get(Calendar.MONTH)-month+1;
+        int dia = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)-day;
+        
+        if(año<0){
+            resultat=-2;
+        }else if(año>150){
+            resultat=-1;
+        }else{
+            if(mes<0){
+                resultat=año-1;
+            }else{
+                resultat=año;
+            }
+        }
+
+        System.out.println(año);
+        System.out.println(mes);
+        System.out.println(dia);
+        
+        
+        return resultat;
+    }
+
+    /**
+     *
+     * @param numero número del que es calcula el factorial
+     * @return retorna el factorial d'un número. Si el nombre es negatiu retorna
+     * -1.
+     */
+    public static double factorial(double numero) {
+
+        if (numero == 0) {
+            return 1;
+        } else {
+            double resultat = numero * factorial(numero - 1);
+            return resultat;
+        }
+    }
 }
